c5b57a7e4f5c7c59888297f67bdda37bcfb07ade
siarhei melnik <siarhei.m.a@gmail.com>
2019-05-18 14:52:33 +0300

    feat: add api

---
+++ services/fields/credentials.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+serverless config credentials --provider aws --key AKIAYASQELTPG66M74OZ --secret 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 --overwrite
--- services/fields/handler.js
+++ services/fields/handler.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports.hello = async (event) => {
+module.exports.match = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
@@ -8,7 +8,18 @@ module.exports.hello = async (event) => {
       input: event,
     }, null, 2),
   };
+};
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+module.exports.coordsUpload = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      headers: {
+        "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+        "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
+      },
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }, null, 2),
+  };
 };
