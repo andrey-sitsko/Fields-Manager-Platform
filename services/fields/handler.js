6452dbb612056b2e6d3d841d966b7882bb8ee3c8
siarhei melnik <siarhei.m.a@gmail.com>
2019-05-18 18:02:10 +0300

    feat: add coords

--- services/fields/handler.js
+++ services/fields/handler.js
@@ -2,17 +2,30 @@
 
 const AWS = require('aws-sdk');
 const http = require('http');
-const crypto = require("crypto");
-const { promisify } = require("util");
-
+const crypto = require('crypto');
+const { promisify } = require('util');
 const s3Client = new AWS.S3();
 
-const s3Upload = promisify(s3Client.putObject.bind(s3Client))
+const s3Upload = promisify(s3Client.putObject.bind(s3Client));
+const s3Get = promisify(s3Client.getObject.bind(s3Client));
 
 const CORS = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers': '*',
   'Access-Control-Allow-Methods': '*'
+};
+
+const mock = {
+  area: [
+    {
+      "lat": 51.520173035107824,
+      "lng": -0.08995056152343751
+    },
+    {
+      "lat": 51.520173035107824,
+      "lng": -0.09012222290039064
+    }
+  ]
 };
 
 const s3FieldParams = (fieldId, body) => ({
@@ -24,7 +37,21 @@ const s3FieldParams = (fieldId, body) => ({
 });
 
 module.exports.match = async (event) => {
-  console.log(event);
+  const data = event.Records[0].s3;
+  const bucketName = data.bucket.name;
+  const objectName = data.object.key;
+
+  const fieldCoordsParams = {
+    Bucket: bucketName, 
+    Key: objectName,
+  }
+
+  const { Body } = await s3Get(fieldCoordsParams);
+
+  const fieldCoords = JSON.parse(Body);
+
+  console.log(fieldCoords);
+
 
   return {
     statusCode: 200,
@@ -38,7 +65,7 @@ module.exports.fieldCreate = async (event) => {
     const data = JSON.parse(event.body || '{}');
     const id = crypto.randomBytes(16).toString("hex");
     await s3Upload(
-      s3FieldParams(id, data)
+      s3FieldParams(id, mock.area)
     );
 
     return {
