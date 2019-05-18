1dfd3cf4fbe15271f6617b6b8452f2c8701c8e9b
siarhei melnik <siarhei.m.a@gmail.com>
2019-05-18 15:57:05 +0300

    feat: update api

--- services/fields/handler.js
+++ services/fields/handler.js
@@ -1,4 +1,13 @@
 'use strict';
+const aws = require('aws-sdk');
+const http = require('http');
+const crypto = require("crypto");
+
+const CORS = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': '*',
+  'Access-Control-Allow-Methods': '*'
+};
 
 module.exports.match = async (event) => {
   return {
@@ -10,16 +19,34 @@ module.exports.match = async (event) => {
   };
 };
 
-module.exports.coordsUpload = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      headers: {
-        "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
-        "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
-      },
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
-  };
+module.exports.fieldCreate = async (event) => {
+  console.log(event.body);
+
+  try {
+    const data = JSON.parse(evt.body || '{}');
+    const id = crypto.randomBytes(16).toString("hex");
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        headers: { ...CORS },
+        data: {
+          fieldId: id,
+          status: 'OK',
+        }
+      }),
+    };
+  } catch (e) {
+    console.log(e);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        headers: { ...CORS },
+        data: {
+          status: 'ERROR',
+        }
+      }),
+    };
+
+  }
 };
