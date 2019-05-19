3d829224f48ae867ef79f9b739a0d7a47375b932
Aliaksei Belmach <livorni1@gmail.com>
2019-05-19 10:14:45 +0300

    Added resources page

--- src/site-modules/my-resources/my-resources.jsx
+++ src/site-modules/my-resources/my-resources.jsx
@@ -1,5 +1,50 @@
 import React from 'react';
+import { Card } from '../shared/components/card/card';
+import { Link } from 'react-router-dom';
+
+import './my-resources.scss';
+
+const MY_RESOURCES_MAP = [
+  {
+    label: 'Weather Stations',
+    href: '#',
+    className: 'weather-stations',
+  },
+  {
+    label: 'Agrotechnical Resources',
+    href: '#',
+    className: 'agrotechnical-resources',
+  },
+  {
+    label: 'Aerial Survey',
+    href: '#',
+    className: 'aerial-survey',
+  },
+  {
+    label: 'Fertilizers and Protection',
+    href: '#',
+    className: 'fertilizers',
+  },
+  {
+    label: 'Human Resources',
+    href: '#',
+    className: 'human-resources',
+  },
+]
 
 export function MyResources() {
-  return 'MyResources';
+  return (
+    <Card className="my-resources-card" hasCloseBtn>
+      <div className="d-flex">
+        {
+          MY_RESOURCES_MAP.map(({ label, href, className }) => 
+            <Link className="resource-link medium d-block" key={href} to={href}>
+              <div className="mb-1">{label}</div>
+              <div className={`image ${className}`} />
+            </Link>
+          )
+        }
+      </div>
+    </Card>
+  )
 }
